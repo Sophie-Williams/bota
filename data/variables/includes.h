ab55ef7067512580a0c757320bd192a232c7d724
Gleb23 <glebsharnenko@gmail.com>
2017-11-17 00:16:51 +0300

    Update includes.h
    add #include "status.h"

--- data/variables/includes.h
+++ data/variables/includes.h
@@ -11,31 +11,32 @@
 // *** DATA ***
 // variables/
 #include "variables.h"						// first, we need init data/variables
-#include "macros.h"							// there a 3 files: includes.h, macros.h, variables.h
+#include "macros.h"					  		// there a 3 files: includes.h, macros.h, variables.h
 // /class/sysClass
 // logs
-#include "log.h"							// log class
-#include "clients.h"						// there must be a clients
-#include "AIagent.h"						// there must be a AI
-// defaultParts								// default parts, maybe need delete this
+#include "log.h"						    	// log class
+#include "clients.h"				  		// there must be a clients
+#include "AIagent.h"				   		// there must be a AI
+// defaultParts						    		// default parts, maybe need delete this
 // minor parts
 #include "coordinates.h"					// coordinate
 #include "healthPoints.h"					// health points.
-#include "material.h"						// material. Wood etc
-#include "OFD.h"							// damage system (base)
+#include "material.h"			  			// material. Wood etc
+#include "OFD.h"			    				// damage system (base)
+#include "status.h"               // status of creature/ item etc. If false - prepare to delete.
 // major parts
 #include "worldParts.h"						// parts of wolrd
 // Entity(minor)
-#include "creature.h"						// active objects (have AI or client module)
-#include "effect.h"							// effect. Area (buff, damage, heal etc)
-#include "item.h"							// All items can be equiped
-#include "object.h"							// Objects - part of map (static) and moving (dynamic)
+#include "creature.h"				  		// active objects (have AI or client module)
+#include "effect.h"						  	// effect. Area (buff, damage, heal etc)
+#include "item.h"					    		// All items can be equiped
+#include "object.h"						  	// Objects - part of map (static) and moving (dynamic)
 // there some collections:
 #include "collections.h"					// collection of entity(minor). Maybe need delete this
 // Entity(major)
-#include "map"								// Local area. Maybe need delete this. Or map can be header of collections of some boolshit.
-#include "region.h"							// True local area.
-#include "world.h"							// Global entity. Not a god object.
+#include "map"							    	// Local area. Maybe need delete this. Or map can be header of collections of some boolshit.
+#include "region.h"						  	// True local area.
+#include "world.h"						  	// Global entity. Not a god object.
 // loop
 // logic
 // physic

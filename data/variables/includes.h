c447c9279efa7b3a64be1cd90b5cd2ff91472fd8
Gleb23 <glebsharnenko@gmail.com>
2017-12-02 18:30:44 +0300

    Update and rename includes.h to data/variables/includes.h

--- data/variables/includes.h
+++ data/variables/includes.h
@@ -1,5 +1,5 @@
 #pragma once
-class Creature;						// there start f.d. classes
+class Creature;							// there start f.d. classes
 class World;
 class Effect;
 class Item;
@@ -24,33 +24,33 @@ class Universe;
 #define GLEW_STATIC						// openGL stuff
 #include <GLEW/glew.h>
 #include <GLFW/glfw3.h>
-										// *** default stuff end here ***
-										// data/ start
+								// *** default stuff end here ***
+								// data/ start
 #include "macro.h"						// macros stuff
-										// classes
+								// classes
 #include "status.h"						// Shit for major or minor classes
 #include "health.h"						
 #include "parts.h"
 #include "coordinate.h"
 
-										// major/minor classes
-#include "creature.h"					// minor
+								// major/minor classes
+#include "creature.h"						// minor
 #include "world.h"						// major
 #include "effect.h"						// minor
 #include "item.h"						// minor
 #include "map.h"						// part of world class
 #include "object.h"						// minor
-#include "collections.h"				// classes of vector with minor classes
-#include "universe.h"					// main class
+#include "collections.h"					// classes of vector with minor classes
+#include "universe.h"						// main class
 										
-										// end of data/ and start of engine/
+								// end of data/ and start of engine/
 #include "input.h"
-#include "OGLWindow.h"					// openGL includes starts here
+#include "OGLWindow.h"						// openGL includes starts here
 #include "openGL.h"						// main window class of openGL
 
-										// engine includes start there
+								// engine includes start there
 #include "loop.h"						// loop.h is main loop.
 #include "engine.h"						// engine launch there. engine(). Giant struct with parameters
-										// go in future
+								// go in future
 
 

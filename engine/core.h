a037c9dc499e7e1f23da5e33d5f4c2ae1c90c9f3
Gleb23 <glebsharnenko@gmail.com>
2017-12-04 08:21:00 +0300

    Update core.h

--- engine/core.h
+++ engine/core.h
@@ -1,1 +1,37 @@
 
+#pragma once
+
+
+
+int coreLoop(Universe universe)
+{
+	// load universe etc.
+	// load save
+
+	// if no save - create new world
+
+
+	
+	while (coreLoopStatus == true)						// main loop
+	{
+		// select world
+
+		// do action with creature (logic)
+
+		// do action with item (logic)
+
+		// do action with effect (logic)
+
+		// do action with object (logic)
+
+		// do action with world (logic)
+
+
+		// magic
+
+		// joins
+
+		
+	}
+	return NULL;
+}
